ca2cc5df2184703b0c72267f2f42be6bcb595f0b
Altan Acun <acunaltan@gmail.com>
2020-07-11 11:43:26 +0300

    .

---
+++ rootFinder Project/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(49, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(49, 169);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(87, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(362, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 286);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(362, 114);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(16, 39);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(84, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Secant";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(106, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bisection";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(213, 39);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(165, 24);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Newton-Raphson";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = " Equation:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "X0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "X1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Select a Method:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(8, 417);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Error:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Root:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 450);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(362, 105);
+            this.richTextBox2.TabIndex = 19;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(12, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(323, 26);
+            this.textBox3.TabIndex = 42;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(340, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 20);
+            this.label7.TabIndex = 45;
+            this.label7.Text = "= 0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label8.Location = new System.Drawing.Point(103, 81);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 16);
+            this.label8.TabIndex = 46;
+            this.label8.Text = "(For Exp: 3*x+3^2)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 156);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(186, 20);
+            this.label9.TabIndex = 47;
+            this.label9.Text = "Derivative of Equation";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(12, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(327, 26);
+            this.textBox4.TabIndex = 48;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label10.Location = new System.Drawing.Point(200, 156);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 16);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "(For Exp: 3*x+3*3)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 567);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(403, 606);
+            this.MinimumSize = new System.Drawing.Size(403, 606);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Root and Error Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ rootFinder Project/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+            
+          
+
+           
+
+        }
+
+        //NEWTON RAPHSON
+
+         double epsilonNewton = 0.001;
+         double funcnewton(double x)
+        {
+            //READING EQUATION
+            string function = textBox3.Text;
+
+            var xinserter = function.Replace("x", Convert.ToString(x));
+            Mathos.Parser.MathParser parser = new Mathos.Parser.MathParser();
+            double result = parser.Parse(xinserter);
+
+            return result;
+
+        }
+         double derivative(double x)
+        {
+            //READING EQUATION
+            string function = textBox4.Text;
+            var xinserter = function.Replace("x", Convert.ToString(x));
+            Mathos.Parser.MathParser parser = new Mathos.Parser.MathParser();
+            double result = parser.Parse(xinserter);
+            return result;
+        }
+
+        public void newtonRaphson(double x)
+        {
+            double h = funcnewton(x) / derivative(x);
+            while (Math.Abs(h) >= epsilonNewton)
+            {
+                h = funcnewton(x) / derivative(x);
+
+                x = x - h;
+            }
+
+           
+           richTextBox1.Text = "The value of the root is : " + Math.Round(x * 100.0) / 100.0;
+
+            //CALCULATING THE ERROR PERCENTAGE
+            double errorStep1 = (Math.Round(x * 100.0) / 100.0 - x) / Math.Round(x * 100.0) / 100.0;
+            double errorStep2 = errorStep1 * 100;
+            richTextBox2.Text = "The absolute error percentage is : " + errorStep2 + "%";
+        }
+
+
+        //SECANT
+        float f(float x)
+        {
+            //READING EQUATION
+            float f = 0;
+
+            string function = textBox3.Text;
+
+            var xinserter = function.Replace("x", Convert.ToString(x));
+            Mathos.Parser.MathParser parser = new Mathos.Parser.MathParser();
+            double result = parser.Parse(xinserter);
+            f = float.Parse(result.ToString());     
+            return f;
+        }
+        public void secant(float x1, float x2,
+                       float E)
+
+        {
+            float n = 0, xm, x0, c;
+            if (f(x1) * f(x2) < 0)
+            {
+                do
+                {
+
+                    x0 = (x1 * f(x2) - x2 * f(x1))
+                        / (f(x2) - f(x1));
+
+                    c = f(x1) * f(x0);
+                    x1 = x2;
+                    x2 = x0;
+
+                    n++;
+
+                    if (c == 0)
+                        break;
+                    xm = (x1 * f(x2) - x2 * f(x1))
+                        / (f(x2) - f(x1));
+
+                }
+
+                while (Math.Abs(xm - x0) >= E);
+
+                richTextBox1.Text = "Root of the given equation = " + x0 + "\n";
+            
+
+
+
+                richTextBox1.AppendText("Number of iterations = " + n);
+            }
+
+            else {
+                richTextBox1.AppendText("Can not find a root in the given inteval");
+                }
+            //CALCULATING THE ERROR PERCENTAGE
+            double errorStep1 = (x2 - x1) / x2;
+            double errorStep2 = errorStep1 * 100;
+            richTextBox2.Text = "The absolute error percentage is : " + errorStep2 + "%";
+
+
+        }
+
+
+        // BISECTION
+
+         float EPSILON = (float)0.01;
+
+         double funcbisection(double x)
+        {
+
+
+            //READING EQUATION
+            
+            string function = textBox3.Text;
+
+            var xinserter = function.Replace("x", Convert.ToString(x));
+            Mathos.Parser.MathParser parser = new Mathos.Parser.MathParser();
+            double result = parser.Parse(xinserter);
+
+            return result;
+
+        }
+
+         void bisection(double a, double b)
+        {
+            if (funcbisection(a) * funcbisection(b) >= 0)
+            {
+                richTextBox1.Text = "You have not assumed right X0 and X1";
+                return;
+            }
+
+            double c = a;
+            while ((b - a) >= EPSILON)
+            {
+                c = (a + b) / 2;
+
+                if (funcbisection(c) == 0.0)
+                    break;
+
+                else if (funcbisection(c) * funcbisection(a) < 0)
+                    b = c;
+                else
+                    a = c;
+            }
+
+            richTextBox1.Text = "The value of root is : " + c;
+            //CALCULATING THE ERROR PERCENTAGE
+            double errorStep1 = (c - b)/b;
+            double errorStep2 = errorStep1 * 100;
+            richTextBox2.Text ="The absolute error percentage is : " + errorStep2 + "%";
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+		{
+            textBox4.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+
+
+        }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+            if (radioButton1.Checked == true && textBox1.Text != "" && textBox2.Text != "")
+            {
+                float x1 = Convert.ToInt32(textBox1.Text);
+                float x2 = Convert.ToInt32(textBox2.Text);
+                float E = 0.0001f;
+                secant(x1, x2, E);
+            }
+            else if (radioButton2.Checked == true && textBox1.Text != "" && textBox2.Text != "")
+            {
+                double a = Convert.ToInt32(textBox1.Text), b = Convert.ToInt32(textBox2.Text);
+                bisection(a, b);
+            }
+            else if (radioButton3.Checked == true && textBox1.Text != "" && textBox4.Text != "")
+            {
+                double x0 = Convert.ToDouble(textBox1.Text);
+                newtonRaphson(x0);
+            }
+            else
+            {
+                MessageBox.Show("Please fill & check all entries");
+            }
+         
+		}
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton3.Checked == true)
+            {
+                textBox2.Text = "";
+                textBox4.Text = "";
+                textBox2.Visible = false;
+                label3.Visible = false;
+                textBox4.Visible = true;
+                label9.Visible = true;
+                label10.Visible = true;
+            }
+            else if (radioButton3.Checked == false)
+            {
+                textBox2.Visible = true;
+                label3.Visible = true;
+                textBox4.Visible = false;
+                label9.Visible = false;
+                textBox2.Text = "";
+                textBox4.Text = "";
+                label10.Visible = false;
+
+            }
+        }
+
+      
+    }
+}
